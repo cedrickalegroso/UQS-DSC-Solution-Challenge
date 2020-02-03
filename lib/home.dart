532bcab2c4153b0fc1f8132d4e2b19bdd2af6a50
unor12 <47923138+unor12@users.noreply.github.com>
2020-02-03 22:08:46 +0800

    Revert "Update home.dart"
    This reverts commit 77b59870d56329f65d0c537e4e0c8b5a29d9dcff.

--- lib/home.dart
+++ lib/home.dart
@@ -1,73 +1,99 @@
 import 'package:flutter/material.dart';
-//import 'package:provider/provider.dart';
 import 'package:uqsbeta/authservice.dart';
-import 'package:uqsbeta/customlisttile.dart';
 
-class Homepage extends StatefulWidget {
-  @override
-  _HomepageState createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  
-
+class Homepage extends StatelessWidget {
+  final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    final AuthService _auth = AuthService();
-    //final service = Provider.of<List<Service>>(context);
     return new Scaffold(
-        appBar: new AppBar(
-          title: Text(""),
-          backgroundColor: Colors.lightBlueAccent,
-          elevation: 0,
-        ),
-        //drawer
-        drawer: new Drawer(
-            child: new ListView(
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: <Color>[
-                Colors.lightBlueAccent,
-                Colors.lightBlue,
-              ])),
-              child: Container(
-                  child: Column(
-                children: <Widget>[
-                  Material(
-                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                    elevation: 10,
-                  ),
-                  Text("Flutter",
-                      style: TextStyle(color: Colors.white, fontSize: 20.0)),
-                ],
-              )),
-            ),
-            CostumListile(Icons.person, 'Profile', () => {}),
-            CostumListile(Icons.notifications, 'Notification', () => {}),
-            CostumListile(Icons.settings, 'Settings', () => {}),
-            CostumListile(Icons.help, 'Help', () => {}),
-            CostumListile(Icons.lock, 'Log out', () async {
-              await _auth.signOut();
-              Navigator.of(context).pushReplacementNamed('/login');
-            }),
-          ],
-        )),
-        body: Container(
-          child: SingleChildScrollView(
-            child: Column(children: <Widget>[
-              SingleChildScrollView(
-                padding: EdgeInsets.all(10),
-                scrollDirection: Axis.horizontal,
-                /*child: ListView.builder(
-                  itemCount: service.length, //depende kung pila ka service ang ginapilahan ni user
-                  itemBuilder: (context, index) {
-                    return ServiceTile(service[index])
-                  }
-                )*/
-              )
-            ]),
+      appBar: new AppBar(
+        title: Text(""),
+        backgroundColor: Colors.lightBlueAccent,
+        elevation: 0,
+      ),
+      //drawer
+      drawer: new Drawer(
+          child: new ListView(
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: <Color>[
+              Colors.lightBlueAccent,
+              Colors.lightBlue,
+            ])),
+            child: Container(
+                child: Column(
+              children: <Widget>[
+                Material(
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  elevation: 10,
+                ),
+                Text("Flutter",
+                    style: TextStyle(color: Colors.white, fontSize: 20.0)),
+              ],
+            )),
           ),
-        ));
+          CostumListile(Icons.person, 'Profile', () => {}),
+          CostumListile(Icons.notifications, 'Notification', () => {}),
+          CostumListile(Icons.settings, 'Settings', () => {}),
+          CostumListile(Icons.help, 'Help', () => {}),
+          CostumListile(Icons.lock, 'Log out', () async {
+            await _auth.signOut();
+            Navigator.of(context).pushReplacementNamed('/login');
+          }),
+        ],
+      )),
+      body: Container(
+          child: Center(
+        child: Text("Home Page"),
+      )),
+    );
+  }
+}
+
+class CostumListile extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Function onTop;
+  CostumListile(this.icon, this.text, this.onTop);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(8.0, 0, 8, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(color: Colors.grey.shade400),
+          ),
+        ),
+        child: InkWell(
+          splashColor: Colors.lightBlueAccent,
+          onTap: onTop,
+          child: Container(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(icon),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        text,
+                        style: TextStyle(
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(Icons.arrow_right),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
