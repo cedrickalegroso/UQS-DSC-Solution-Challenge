a198f39a8fee075a75ac76e742b3a4512327e834
unor12 <47923138+unor12@users.noreply.github.com>
2020-02-03 22:09:20 +0800

    Revert "Merge branch 'login-branch' of https://github.com/cedrickalegroso/UQS-DSC-Solution-Challenge into login-branch"
    This reverts commit d73547a2b62ed92cf4ffe3ba099c07aa23f4e76e, reversing
    changes made to 77b59870d56329f65d0c537e4e0c8b5a29d9dcff.

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,29 @@
 import 'package:flutter/material.dart';
-import 'Auth.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:uqsbeta/Splashscreen.dart';
+import 'package:uqsbeta/authservice.dart';
+import 'package:uqsbeta/models/user.dart';
+import 'package:uqsbeta/route.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      brightness: Brightness.light,
-      primaryColor: Colors.lightBlue[800],
-      accentColor: Colors.redAccent[300],
-    ),
-    routes: {
-      // When navigating to the "/" route, build the FirstScreen widget.
-      '/AuthPage': (context) => AuthPage(),
-      // When navigating to the "/second" route, build the SecondScreen widget.
-      '/register': (context) => Register(),
-      '/accountSetup': (context) => AccountSetup(),
-    },
-    
-    home: new MyApp(),
-  ));
-}
+void main() => runApp(MyApp());
 
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-
-
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  //This is the root widget
   @override
   Widget build(BuildContext context) {
-    return new SplashScreen(
-      title: new Text(
-        'Unified Qeueing System',
-        style: new TextStyle(
-            fontWeight: FontWeight.bold, fontSize: 20.0, color: Colors.grey),
+    //wrapped the root widget with streamprovider to grant the whole widget tree access to the data provided by the stream
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        onGenerateRoute: RouteGenerator.generateRoute,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.lightBlue[800],
+          accentColor: Colors.redAccent[300],
+        ),
+        home: Splashscreen(),
       ),
-      seconds: 5,
-      navigateAfterSeconds: AuthPage(),
-      image: new Image.asset('assets/launch_image.png'),
-      backgroundColor: Colors.white,
-      styleTextUnderTheLoader: new TextStyle(),
-      photoSize: 150.0,
-      onClick: () => print("Flutter Egypt"),
-      loaderColor: Colors.white,
     );
   }
 }
