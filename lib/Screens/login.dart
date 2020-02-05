32619b1a54d49d27b8160fe77ae0d3c5a1ecc55f
unor12 <47923138+unor12@users.noreply.github.com>
2020-02-05 12:38:05 +0800

    Update login.dart

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -14,9 +14,6 @@ class _AuthPageState extends State<AuthPage> {
   final AuthService _auth = AuthService();
   String _password = '';
   String _email = '';
-<<<<<<< Updated upstream
-  bool loading = false;
-=======
 
   //show snackbar method
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -43,19 +40,9 @@ class _AuthPageState extends State<AuthPage> {
         });
   }
 
->>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     // @carl gin update ko dire nga part gin kuha ko na ang appbar kay pwedi ya ma click pabalik sa Screensplash
-<<<<<<< Updated upstream
-    return loading
-        ? Loading()
-        : Scaffold(
-            resizeToAvoidBottomPadding: true,
-            backgroundColor: Colors.lightBlueAccent,
-            body: Stack(
-              fit: StackFit.expand,
-=======
     return Scaffold(
         key: _scaffoldKey,
         resizeToAvoidBottomPadding: true,
@@ -65,7 +52,6 @@ class _AuthPageState extends State<AuthPage> {
           children: <Widget>[
             Column(
               mainAxisAlignment: MainAxisAlignment.start,
->>>>>>> Stashed changes
               children: <Widget>[
                 Expanded(
                     flex: 5,
@@ -91,37 +77,9 @@ class _AuthPageState extends State<AuthPage> {
               child: SingleChildScrollView(
                 child: Column(
                   children: <Widget>[
-<<<<<<< Updated upstream
-                    Expanded(
-                        flex: 5,
-                        child: Container(
-                          child: Column(
-                            
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(padding: EdgeInsets.only(top: 50.0)),
- 
-                              Text(
-                                'UQS',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 30.0,
-                                    fontWeight: FontWeight.bold),
-                              )
-                            ],
-                          ),
-                        )),
-                  ],
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: SingleChildScrollView(
-                    child: Column(
-=======
                     Column(
                       mainAxisAlignment: MainAxisAlignment.end,
                       crossAxisAlignment: CrossAxisAlignment.start,
->>>>>>> Stashed changes
                       children: <Widget>[
                         Padding(
                           padding: const EdgeInsets.only(left: 20, right: 20),
@@ -201,103 +159,6 @@ class _AuthPageState extends State<AuthPage> {
                               ),
                             ),
                             SizedBox(height: 8),
-<<<<<<< Updated upstream
-                          ],
-                        ),
-                        Container(
-                          alignment: Alignment.bottomCenter,
-                          margin: EdgeInsets.all(15),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(25)),
-                            color: Colors.white,
-                          ),
-                          child: Form(
-                            key: _formKey,
-                            child: Column(
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: TextFormField(
-                                    autofocus: false,
-                                    textAlign: TextAlign.left,
-                                    validator: (value) => value.isEmpty
-                                        ? 'Email address is required'
-                                        : null,
-                                    onChanged: (val) {
-                                      setState(() => _email = val.trim());
-                                    },
-                                    onSaved: (value) => _email = value.trim(),
-                                    keyboardType: TextInputType.emailAddress,
-                                    decoration: InputDecoration(
-                                      hintText: 'email@gmail.com',
-                                      labelText: "Email Address",
-                                      contentPadding: EdgeInsets.all(6),
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: TextFormField(
-                                    autofocus: false,
-                                    textAlign: TextAlign.left,
-                                    validator: (value) => value.isEmpty
-                                        ? 'Password is required'
-                                        : null,
-                                    onChanged: (val) {
-                                      setState(() => _password = val);
-                                    },
-                                    onSaved: (value) =>
-                                        _password = value.trim(),
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                        hintText: '6-12 characters',
-                                        labelText: "Password",
-                                        contentPadding: EdgeInsets.all(6)),
-                                  ),
-                                ),
-                                SizedBox(height: 8),
-                                Container(
-                                  padding:
-                                      EdgeInsets.only(left: 100, right: 100),
-                                  child: RaisedButton(
-                                    color: Colors.lightBlueAccent,
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    onPressed: () async {
-                                      //using the built in func of validating forms, this returns false if form is invalid
-                                      if (_formKey.currentState.validate()) {
-                                        //set loading to true to show loading screen while waiting for the result from firebaseauth
-                                        setState(() => loading = true);
-                                        //call the sign in function under AuthService()
-                                        dynamic result = await _auth
-                                            .signInEmail(_email, _password);
-                                        if (result == null) {
-                                          //set loading to false to return to sign in page if user fails to sign in
-                                          setState(() => loading = false);
-                                        } else {
-                                          //proceeds to homepage otherwise
-                                          Navigator.of(context)
-                                              .pushReplacementNamed('/home');
-                                        }
-                                      }
-                                    },
-                                    child: Text(
-                                      'Login',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(height: 10),
-                                Container(
-                                  child: GestureDetector(
-                                    //brings the user to the signup page when tapped
-                                    onTap: () {
-=======
                             Container(
                               padding: EdgeInsets.only(left: 100, right: 100),
                               child: RaisedButton(
@@ -322,7 +183,6 @@ class _AuthPageState extends State<AuthPage> {
                                       _showSnackBar();
                                     } else {
                                       //proceeds to homepage otherwise
->>>>>>> Stashed changes
                                       Navigator.of(context)
                                           .pushReplacementNamed('/home');
                                     }
