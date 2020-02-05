82908003376a73bc322a2c25abd98b0d93220615
unor12 <47923138+unor12@users.noreply.github.com>
2020-02-05 12:36:25 +0800

    Update loading.dart

--- lib/Miscellaneous/loading.dart
+++ lib/Miscellaneous/loading.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightBlueAccent[100],
+    
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
       child: Center(
         child: SpinKitChasingDots(
           color: Colors.lightBlueAccent,
