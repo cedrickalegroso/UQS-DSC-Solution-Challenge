35d2fab026bfefedaf5f13283a426b249fb8bbd7
unor12 <47923138+unor12@users.noreply.github.com>
2020-02-15 22:22:59 +0800

    Update wrapper.dart
    modified to fix authentication flow

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uqsbeta/Miscellaneous/loading.dart';
 import 'package:uqsbeta/Screens/Home/home.dart';
 import 'package:uqsbeta/Screens/login.dart';
 import 'package:uqsbeta/Models/user.dart';
@@ -10,18 +11,24 @@ class Wrapper extends StatelessWidget {
     final authService = AuthService();
 
     //checking if user is logged in then return either authpage or homescreen
+    //uses a streambuilder to build widgets according to the authentication state of the user
     return StreamBuilder<User>(
       stream: authService.user,
       builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-        final User user = snapshot.data;
-
-        if (user == null) {
-          print('No user is logged in');
-          return AuthPage();
+        if (snapshot.connectionState == ConnectionState.active) {
+          final User user = snapshot.data;
+          if (user == null) {
+            print('No user is logged in');
+            return AuthPage();
+          }
+          print('User with uid: ${user.uid} is logged in');
+          return Homepage();
+        } else {
+          return Container(
+            child: Loading()
+          );
         }
-        print('User with uid: ${user.uid} is logged in');
-        return Homepage(uid: user.uid);
       },
-    ); //user != null ? Homepage(uid: user.uid) : AuthPage(); //=> if true, the data of user is passed to Homepage
+    ); 
   }
 }
