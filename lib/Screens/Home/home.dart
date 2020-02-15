14f89cb7d27bc5c314f4ab75a3a6f7300b0d41e3
unor12 <47923138+unor12@users.noreply.github.com>
2020-02-15 22:23:27 +0800

    Update home.dart

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:uqsbeta/Models/user.dart';
 import 'package:uqsbeta/Screens/Home/activeTickets.dart';
 import 'package:uqsbeta/Screens/Home/customlisttile.dart';
 import 'package:uqsbeta/Models/service.dart';
@@ -9,43 +8,33 @@ import 'package:uqsbeta/Screens/Home/serviceList.dart';
 import 'package:uqsbeta/Screens/Home/userTile.dart';
 import 'package:uqsbeta/Services/authservice.dart';
 import 'package:uqsbeta/Services/serviceDatabase.dart';
-import 'package:uqsbeta/Services/userDatabase.dart';
 
 class Homepage extends StatefulWidget {
-  final uid;
-  Homepage({this.uid});
   @override
-  _HomepageState createState() => _HomepageState(uid);
+  _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
-  dynamic uid;
-
-  _HomepageState(this.uid);
   @override
   Widget build(BuildContext context) {
     final AuthService _auth = AuthService();
-    //create an instance based on the data from the stream
-    //this is then used as parameter para mag set up sang stream para sa userData
-   
-
     //wrapped the whole scaffold widget with streamprovider to grant the whole widget tree access to the data provided by the stream
     return MultiProvider(
       providers: [
         StreamProvider<List<Service>>.value(value: ServiceDatabase().service),
-        StreamProvider<UserData>.value(
-            value: DatabaseService(uid:uid).userData),
       ],
       child: SafeArea(
         child: new Scaffold(
-          extendBody: true,
-            appBar: PreferredSize(preferredSize: Size.fromHeight(20),child: AppBar(backgroundColor: Colors.transparent, elevation: 0)),
+            extendBody: true,
+            appBar: PreferredSize(
+                preferredSize: Size.fromHeight(20),
+                child:
+                    AppBar(backgroundColor: Colors.transparent, elevation: 0)),
             resizeToAvoidBottomPadding: true,
             backgroundColor: Colors.lightBlueAccent,
             drawer: Drawer(child: DrawerList(auth: _auth)),
             body: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 ListTile(
